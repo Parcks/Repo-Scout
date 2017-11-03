ccfdf544aa5142ea38576a1616e296b95046792a
JValck <jasper_de_valck@hotmail.com>
2017-11-03 17:11:49 +0100

    INITIAL COMMIT First commit Changes: 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/repo_scout.iml 	new file:   .idea/workspace.xml 	new file:   main.py 	new file:   network/__init__.py 	new file:   network/factory/__init__.py 	new file:   network/factory/scout_factory.py 	new file:   network/scout/__init__.py 	new file:   network/scout/findable.py 	new file:   network/scout/github_scout.py 	new file:   service/__init__.py 	new file:   service/repo_scout.py

---
+++ network/scout/github_scout.py
@@ -0,0 +1,41 @@
+"""
+Repo Scout
+Copyright (C) 2017  JValck - Setarit
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Setarit - parcks[at]setarit.com
+"""
+from __future__ import absolute_import
+from network.scout.findable import Findable
+import requests
+
+
+class GitHubScout(Findable):
+    def __init__(self):
+        super(GitHubScout, self).__init__()
+        ENDPOINT_URL = "https://api.github.com/repos/"
+        ENDPOINT_URL_SUFFIX = "/contents"
+
+
+    def find(self, repo_owner, repo_name, file):
+        url = GitHubScout.ENDPOINT_URL + repo_owner + "/" + repo_name + GitHubScout.ENDPOINT_URL_SUFFIX
+        self.find_in_request_contents(url, file)
+
+    def find_in_request_contents(self, url):
+        request = requests.get(url)
+        if request.status_code == 200:
+            result = request.json()
+            for entry in result:
